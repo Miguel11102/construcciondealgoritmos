499da79857e69c30e19266d3f7c411df62dba6fc
Your Name <miguelangel11230@gmail>
2024-02-22 11:05:56 -0500

    inicio

---
+++ empleado/empleado.py
@@ -0,0 +1,12 @@
+class   empleado:
+    #codigo
+    '''---------------
+    #atributos
+    ---------------'''
+    nombre=''
+    apellido=''
+    ''' ----------------
+    #1=masculino and 2=femenino
+    ---------------- '''
+    sexo=''
+    salario=0    
